9c373a61b8f65a48ebb0ef55be3e8282786591db
rahulmahajann <rahulmahajan98645@gmail.com>
2021-09-21 16:12:33 +0530

    gitignore added

--- first_app.js
+++ first_app.js
@@ -1,5 +1,19 @@
 // console.log('hello');
 
-const fs = require('fs');
+// const fs = require('fs');
 
-fs.writeFileSync('a.py',"print('hello')")
+// fs.writeFileSync('a.py',"print('hello')")
+
+
+
+setTimeout( () => {
+    console.log('after 5000');
+}, 5000);
+
+console.log('hello');
+console.log('hi');
+
+setTimeout( () => {
+    console.log('after 2000');
+}, 2000);
+    
